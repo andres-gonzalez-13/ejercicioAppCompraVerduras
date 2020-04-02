d3230867c65d728b2576f4dfdaebfaf21be355bb
Usuario <Usuario@DESKTOP-03L9RH7>
2020-04-02 13:36:13 -0500

    carpetas basicas

---
+++ src/run/Run.java
@@ -0,0 +1,18 @@
+package run;
+
+import presentation.Presenter;
+
+/**
+ *
+ * @author andres santiago at andrecksantiago@gmail.com
+ */
+public class Run {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Presenter();
+    }
+    
+}
